a39d31ac24ddc337ba581441328981c9b698fae5
kjaing18 <kjaing18@csu.fullerton.edu>
2016-04-30 20:39:31 -0700

    Finished Assignment05

---
+++ solution--kjaing18/Square.h
@@ -0,0 +1,18 @@
+//
+//  Square.h
+//  Assignment5
+//
+//  Created by Katelyn on 4/22/16.
+//  Copyright © 2016 Katelyn. All rights reserved.
+//
+
+#ifndef Square_h
+#define Square_h
+
+#include "Point.h"
+class Square : public Rectangle
+{
+    
+};
+
+#endif /* Square_h */
