4f947acb0a0a041e6446e4f54f8b10ce9d63915d
kjaing18 <kjaing18@csu.fullerton.edu>
2016-04-30 20:39:47 -0700

    Finished Assignment05

---
+++ solution--kjaing18/Circle.h
@@ -0,0 +1,18 @@
+//
+//  Circle.h
+//  Assignment5
+//
+//  Created by Katelyn on 4/22/16.
+//  Copyright © 2016 Katelyn. All rights reserved.
+//
+
+#ifndef Circle_h
+#define Circle_h
+
+#include "Point.h"
+class Circle : public Ellipse
+{
+    
+};
+
+#endif /* Circle_h */
